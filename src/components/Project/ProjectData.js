38b6cdd8bdfb8f6e02dc8803180a3bb579d5e538
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-12-01 19:12:41 +0600

    project updated

--- src/components/Project/ProjectData.js
+++ src/components/Project/ProjectData.js
@@ -9,23 +9,24 @@ const Projectdata = [
         category: 'react'
     },
     {
-        id: 2,
-        image: 'https://i.ibb.co/h8kFYRY/TechEdu.jpg',
-        title: 'TourX',
-        tech: 'React, API,Material-UI',
-        demo: 'https://tour-x-40f10.web.app/',
-        github: 'https://github.com/DevBobi/TourX-Client-ReactJS',
-        category: 'react'
-    },
-    {
         id: 3,
         image: 'https://i.ibb.co/1RH6q28/Tour-X.jpg',
-        title: 'TechEdu',
+        title: 'TourX',
         tech: 'React, API,Material-UI',
         demo: 'https://ecstatic-euclid-d47142.netlify.app/',
         github: 'https://github.com/DevBobi/TourX-Client-ReactJS',
         category: 'react'
+    },
+    {
+        id: 2,
+        image: 'https://i.ibb.co/h8kFYRY/TechEdu.jpg',
+        title: 'TechEdu',
+        tech: 'React, API,Material-UI',
+        demo: 'https://tour-x-40f10.web.app/',
+        github: 'https://github.com/DevBobi/TourX-Client-ReactJS',
+        category: 'react'
     }
+
     // {
     //     id: 4,
     //     image: 'https://i.ibb.co/4JGdvj8/Denta.jpg',
