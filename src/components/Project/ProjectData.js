85aa746d684ed9e2f77cd2630b6e00bc1a96d194
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-02-18 04:04:56 +0600

    project added

--- src/components/Project/ProjectData.js
+++ src/components/Project/ProjectData.js
@@ -2,30 +2,31 @@ const Projectdata = [
     {
         id: 1,
         image: 'https://i.ibb.co/3WcPbWg/SneakHub.jpg',
-        title: 'SneakHub',
+        title: 'SneakHub (MERN Stack)',
         tech: 'React,Next JS,Tailwind CSS,Next-auth,Firebase,Stripe',
         demo: 'https://sneak-hub.web.app/',
         github: 'https://github.com/DevBobi/SneakHub-Client-ReactJS',
         category: 'react'
     },
     {
+        id: 2,
+        image: 'https://i.ibb.co/X41zp2X/Aerial-Aspects.jpg',
+        title: 'Aerial Aspects (MERN Stack)',
+        tech: 'React, Bootstrap',
+        demo: 'https://aerial-aspects.web.app/',
+        github: 'https://github.com/DevBobi/Aerial-Aspects-Client',
+        category: 'react'
+    },
+    {
         id: 3,
         image: 'https://i.ibb.co/1RH6q28/Tour-X.jpg',
-        title: 'TourX',
+        title: 'TourX (MERN Stack)',
         tech: 'React, API,Material-UI',
         demo: 'https://tour-x-40f10.web.app/',
         github: 'https://github.com/DevBobi/TourX-Client-ReactJS',
         category: 'react'
     },
-    {
-        id: 2,
-        image: 'https://i.ibb.co/h8kFYRY/TechEdu.jpg',
-        title: 'TechEdu',
-        tech: 'React, API,Material-UI',
-        demo: 'https://ecstatic-euclid-d47142.netlify.app/',
-        github: 'https://github.com/DevBobi/TourX-Client-ReactJS',
-        category: 'react'
-    }
+
 
     // {
     //     id: 4,
